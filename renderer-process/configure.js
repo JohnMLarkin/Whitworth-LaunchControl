7fbfd36a3510811b9f2ff7c7858243b780596078
John M. Larkin <jlarkin@whitworth.edu>
2018-07-11 18:13:34 -0700

    Update to new major versions of packages

--- renderer-process/configure.js
+++ renderer-process/configure.js
@@ -2,7 +2,7 @@
 /* const path = require('path') */
 const settings = require('electron-settings')
 
-var numBytesPods = [22, 0, 0, 0, 0, 0, 0];
+var numBytesPods = [28, 0, 0, 0, 0, 0, 0];
 var podTable = [[],[],[],[],[],[]];
 const numPods = podTable.length;
 var dataTypes = [['uint8','char',1,'0 .. 255'],
