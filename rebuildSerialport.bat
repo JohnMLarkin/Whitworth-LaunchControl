346a2369a4796881caabcd0d68e88e90ac066299
John M. Larkin <jlarkin@whitworth.edu>
2018-07-25 22:31:43 -0700

    Updates to make Electron 2.0.x work

---
+++ rebuildSerialport.bat
@@ -0,0 +1,8 @@
+export npm_config_target=2.0.5
+export npm_config_arch=x64
+export npm_config_target_arch=x64
+export npm_config_disturl=https://atom.io/download/electron
+export npm_config_runtime=electron
+export npm_config_build_from_source=true
+
+npm install serialport --save
