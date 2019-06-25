12096b1228e016b5e063b1e4afc2e1aae6cf1d2b
crussell2021 <50681298+crussell2021@users.noreply.github.com>
2019-06-25 13:59:24 -0700

    monitor (again)

---
+++ renderer-process/monitor.js
@@ -0,0 +1,7 @@
+const settings = require('electron-settings');
+
+const dataTypes = require('./dataTypes');
+
+var numBytesPods = [27, 0, 0, 0, 0, 0, 0];
+var podTable = [[],[],[],[],[],[]];
+const numPods = podTable.length;
